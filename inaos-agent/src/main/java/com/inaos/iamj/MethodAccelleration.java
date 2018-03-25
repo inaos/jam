fd5184b9754688a2a794514087733604ed273201
Christian A. Steiner <christian.steiner@inaos.ch>
2018-03-25 18:23:31 +0200

    fixed typo

--- inaos-agent/src/main/java/com/inaos/iamj/MethodAccelleration.java
+++ inaos-agent/src/main/java/com/inaos/iamj/MethodAccelleration.java
@@ -26,12 +26,12 @@ class MethodAccelleration {
     private static final MethodDescription.InDefinedShape TYPE, METHOD, PARAMETERS, LIBRARIES, DISPATCHER, BINARY, SYSTEM_LOAD;
 
     static {
-        TypeDescription accelleration = new TypeDescription.ForLoadedType(Accelleration.class);
+        TypeDescription accelleration = new TypeDescription.ForLoadedType(Acceleration.class);
         TYPE = accelleration.getDeclaredMethods().filter(named("type")).getOnly();
         METHOD = accelleration.getDeclaredMethods().filter(named("method")).getOnly();
         PARAMETERS = accelleration.getDeclaredMethods().filter(named("parameters")).getOnly();
         LIBRARIES = accelleration.getDeclaredMethods().filter(named("libraries")).getOnly();
-        TypeDescription library = new TypeDescription.ForLoadedType(Accelleration.Library.class);
+        TypeDescription library = new TypeDescription.ForLoadedType(Acceleration.Library.class);
         DISPATCHER = library.getDeclaredMethods().filter(named("dispatcher")).getOnly();
         BINARY = library.getDeclaredMethods().filter(named("binary")).getOnly();
         TypeDescription system = new TypeDescription.ForLoadedType(System.class);
@@ -54,7 +54,7 @@ class MethodAccelleration {
                         TypePool.Resolution resolution = typePool.describe(classFile.substring(0, classFile.length() - ".class".length()));
                         if (resolution.isResolved()) {
                             TypeDescription typeDescription = resolution.resolve();
-                            if (typeDescription.getDeclaredAnnotations().isAnnotationPresent(Accelleration.class)) {
+                            if (typeDescription.getDeclaredAnnotations().isAnnotationPresent(Acceleration.class)) {
                                 accellerations.add(new MethodAccelleration(typeDescription, classFileLocator, classLoader));
                             }
                         } else {
@@ -91,18 +91,18 @@ class MethodAccelleration {
     }
 
     ElementMatcher<TypeDescription> type() {
-        AnnotationDescription annotation = typeDescription.getDeclaredAnnotations().ofType(Accelleration.class);
+        AnnotationDescription annotation = typeDescription.getDeclaredAnnotations().ofType(Acceleration.class);
         return is(annotation.getValue(TYPE).resolve(TypeDescription.class));
     }
 
     ElementMatcher<MethodDescription> method() {
-        AnnotationDescription annotation = typeDescription.getDeclaredAnnotations().ofType(Accelleration.class);
+        AnnotationDescription annotation = typeDescription.getDeclaredAnnotations().ofType(Acceleration.class);
         return named(annotation.getValue(METHOD).resolve(String.class)).and(takesArguments(annotation.getValue(PARAMETERS).resolve(TypeDescription[].class)));
     }
 
     List<DynamicType.Unloaded<?>> binaries(ByteBuddy byteBuddy, String folder, String prefix, String extension) {
         List<DynamicType.Unloaded<?>> types = new ArrayList<DynamicType.Unloaded<?>>();
-        AnnotationDescription annotation = typeDescription.getDeclaredAnnotations().ofType(Accelleration.class);
+        AnnotationDescription annotation = typeDescription.getDeclaredAnnotations().ofType(Acceleration.class);
         for (AnnotationDescription library : annotation.getValue(LIBRARIES).resolve(AnnotationDescription[].class)) {
             String resource = folder + "/" + prefix + library.getValue(BINARY).resolve(String.class);
             InputStream in = classLoader.getResourceAsStream(resource + "." + extension);
