fefb7abe6c6078ba448a6c44779f07339dd70145
Rafael Winterhalter <rafael.wth@gmail.com>
2018-05-16 23:07:19 +0200

    Extend build to run tests.

--- make.bat
+++ make.bat
@@ -8,3 +8,4 @@ del /Q inaos-example-lib\src\main\resources\jnisample.dll
 copy build\jnisample.dll inaos-example-lib\src\main\resources\win32-amd64\jnisample.dll
 copy build\example3.dll inaos-example-lib\src\main\resources\win32-amd64\example3.dll
 mvn clean package -DskipTests
+mvn surefire:test
--- make.sh
+++ make.sh
@@ -9,3 +9,4 @@ mkdir -p inaos-example-lib/src/main/resources/linux-amd64
 cp build/libjnisample.so inaos-example-lib/src/main/resources/linux-amd64/libjnisample.so
 cp build/libexample3.so inaos-example-lib/src/main/resources/linux-amd64/libexample3.so
 mvn clean package -DskipTests
+mvn surefire:test
