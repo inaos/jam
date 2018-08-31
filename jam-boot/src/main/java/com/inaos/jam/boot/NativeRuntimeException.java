83f18b0c7fc1df8d62361c1571d3d272d88b2fe9
Rafael Winterhalter <rafael.wth@gmail.com>
2018-08-31 21:40:05 +0200

    Do not add Java 7-specific API.

--- jam-boot/src/main/java/com/inaos/jam/boot/NativeRuntimeException.java
+++ jam-boot/src/main/java/com/inaos/jam/boot/NativeRuntimeException.java
@@ -16,28 +16,33 @@
 
 package com.inaos.jam.boot;
 
-import java.nio.file.Paths;
+import java.io.File;
+import java.io.IOException;
 
 public class NativeRuntimeException extends RuntimeException {
-	public NativeRuntimeException(String message) {
-		super(message);
-	}
-	
-	public void setNativeError(String functionName, String file, int line) {
-		addStackTraceElement(this, functionName, file, line);
-	}
-	
-	private static void addStackTraceElement(Throwable t, String functionName, String file, int line) {
-		StackTraceElement[] currentStack = t.getStackTrace();
-		StackTraceElement[] newStack = new StackTraceElement[currentStack.length+1];
-		
-		System.arraycopy(currentStack, 0, newStack, 1, currentStack.length);
-		
-		// we do this because of windows path names (backslashes and other fun)
-		file = Paths.get(file).normalize().toString();
-		
-		newStack[0] = new StackTraceElement("<native>", functionName, file, line);
-		
-		t.setStackTrace(newStack);
-	}
+
+    public NativeRuntimeException(String message) {
+        super(message);
+    }
+
+    public void setNativeError(String functionName, String file, int line) {
+        addStackTraceElement(this, functionName, file, line);
+    }
+
+    private static void addStackTraceElement(Throwable t, String functionName, String file, int line) {
+        StackTraceElement[] currentStack = t.getStackTrace();
+        StackTraceElement[] newStack = new StackTraceElement[currentStack.length + 1];
+
+        System.arraycopy(currentStack, 0, newStack, 1, currentStack.length);
+
+        // we do this because of windows path names (backslashes and other fun)
+        try {
+            file = new File(file).getCanonicalPath();
+        } catch (IOException ignored) {
+        }
+
+        newStack[0] = new StackTraceElement("<native>", functionName, file, line);
+
+        t.setStackTrace(newStack);
+    }
 }
